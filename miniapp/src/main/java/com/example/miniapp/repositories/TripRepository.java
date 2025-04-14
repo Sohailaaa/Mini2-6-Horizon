3f2c7d03ef57cc9a2bc5cc288882981ee1c9b2f6
john-roufaeil <john.flouis1@gmail.com>
2025-04-15 01:00:58 +0200

    fix: add param to trip respository

--- miniapp/src/main/java/com/example/miniapp/repositories/TripRepository.java
+++ miniapp/src/main/java/com/example/miniapp/repositories/TripRepository.java
@@ -4,6 +4,7 @@ import com.example.miniapp.models.Payment;
 import com.example.miniapp.models.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,9 +13,9 @@ import java.util.List;
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
-    @Query(value = "SELECT * FROM trips WHERE tripDate <= :endDate AND tripDate >= :startDate", nativeQuery = true)
-    List<Trip> findByTripDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+    @Query(value = "SELECT * FROM trips WHERE trip_date BETWEEN :startDate AND :endDate", nativeQuery = true)
+    List<Trip> findByTripDateBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
     @Query(value = "SELECT * FROM trips WHERE captain_id = :captainId", nativeQuery = true)
-    List<Trip> findByCaptainId(Long captainId);
+    List<Trip> findByCaptainId(@Param("captainId") Long captainId);
 }
